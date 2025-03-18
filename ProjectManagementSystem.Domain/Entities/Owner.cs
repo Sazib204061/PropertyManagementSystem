950b56679afe7744754cb4846087ce4d05bdbced
Bhaskor Roy <112170792+Bhaskor28@users.noreply.github.com>
2025-03-18 16:51:27 +0600

    I have added Owner (#3)

---
+++ ProjectManagementSystem.Domain/Entities/Owner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+        public string OwnerName { get; set; } = string.Empty;
+        public int ContactNumber { get; set; }
+        public string Email { get; set; }
+        public ICollection<Property> Property { get; set; }
+
+
+
+
+
+    }
+}
