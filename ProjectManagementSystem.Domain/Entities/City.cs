e84f5d673562511cb02319603e64439444f13a43
BKR70 <bilas.duet.204027@gmail.com>
2025-03-18 14:41:34 +0600

    Division property added

--- ProjectManagementSystem.Domain/Entities/City.cs
+++ ProjectManagementSystem.Domain/Entities/City.cs
@@ -11,5 +11,6 @@ namespace ProjectManagementSystem.Domain.Entities
         public int CityId { get; set; }
         public string CityName { get; set; }
         public string Description { get; set; }
+        public int Division { get; set; }
     }
 }
