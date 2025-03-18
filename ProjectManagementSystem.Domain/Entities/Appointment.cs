51fa13b536c4edaa228c68dd3bd93e99d9e49109
Bhaskor Roy <112170792+Bhaskor28@users.noreply.github.com>
2025-03-18 16:58:29 +0600

    Feature/property entity (#4)
    * Added Appointment
    
    * Propperty Added

---
+++ ProjectManagementSystem.Domain/Entities/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public Property ? Properties { get; set; }
+        public DateTime AppointDate { get; set; }
+        public bool Status { get; set; }
+        bool IsDleted { get; set; }
+
+
+    }
+}
