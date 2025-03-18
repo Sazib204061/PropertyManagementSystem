7699118c6519ebb28de6c81720fa08adb578ebd3
Bhaskor <bhaskorcse520@gmail.com>
2025-03-18 14:37:20 +0600

    Added Description

--- ProjectManagementSystem.Domain/Entities/City.cs
+++ ProjectManagementSystem.Domain/Entities/City.cs
@@ -10,5 +10,6 @@ namespace ProjectManagementSystem.Domain.Entities
     {
         public int CityId { get; set; }
         public string CityName { get; set; }
+        public string Description { get; set; }
     }
 }
