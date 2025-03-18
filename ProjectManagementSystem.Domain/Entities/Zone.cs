382961029bea715ebda4d058455ad4fc35de203c
Mahbub-Suhan <immahbubsuhan2000@gmail.com>
2025-03-18 15:07:18 +0600

    Zone added to PMS

---
+++ ProjectManagementSystem.Domain/Entities/Zone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class Zone
+    {
+        public int ZoneId { get; set; }
+        public string ZoneName { get; set; } = string.Empty;
+        public int CityId { get; set; }
+    }
+}
