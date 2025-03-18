589e0c4f34abeffe17ee595eaeed80033d5d000e
BKR70 <bilas.duet.204027@gmail.com>
2025-03-18 16:38:34 +0600

    Property details and Image model created

---
+++ ProjectManagementSystem.Domain/Entities/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+        public string Path { get; set; } = string.Empty;
+        public string Property { get; set; } = string.Empty;
+        public int DetailsId { get; set; }
+        public PropertyDetails? PropertiesDetails { get; set; }
+    }
+}
---
+++ ProjectManagementSystem.Domain/Entities/PropertyDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class PropertyDetails
+    {
+
+        [Key]
+        public int PropertyDetailsId { get; set; }
+        public string PropertyName { get; set; } = string.Empty;
+        public int PropertyId { get; set; }
+        public int TotalRoom { get; set; }
+        public int Size { get; set; }
+        public int ImageId { get; set; }
+        public ICollection<Image> Images { get; set; } = [];
+    }
+}
