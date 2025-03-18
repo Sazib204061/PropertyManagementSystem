b83b3d5cf52e07974fe39232a76b9a2a12dfbb48
Bhaskor <bhaskorcse520@gmail.com>
2025-03-18 16:43:05 +0600

    Propperty Added

---
+++ ProjectManagementSystem.Domain/Entities/Property.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Entities
+{
+    public class Property
+    {
+        public int PropertyId { get; set; }
+        public string PropertyName { get; set; }
+        public Zone ? Zones { get; set; }
+        public Owner Owners { get; set; }
+        public  PropertyDetails ? PropertyDetails { get; set; }
+        public string ?  Status { get; set; }
+        public string? Type { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
