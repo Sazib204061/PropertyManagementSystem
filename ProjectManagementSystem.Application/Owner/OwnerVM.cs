9cea6f8886df47ea089908654ab0b5672401d5fa
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-03-19 16:35:24 +0600

    Impliment Owner Services

---
+++ ProjectManagementSystem.Application/Owner/OwnerVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Owner
+{
+    public class OwnerVM
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //public string? ContactNumber { get; set; }
+    }
+}
