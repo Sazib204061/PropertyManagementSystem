23c71ff0f0d64e1efbe5cb114671d5430bc8e36d
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-03-21 14:56:37 +0600

    nn

--- ProjectManagementSystem.Domain/Entities/City.cs
+++ ProjectManagementSystem.Domain/Entities/City.cs
@@ -9,7 +9,6 @@ namespace ProjectManagementSystem.Domain.Entities
 {
     public class City : BaseEntity
     {
-        //public int CityId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
     }
